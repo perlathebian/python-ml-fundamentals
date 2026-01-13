d58979b89e8b79cc50b2a4564af6a30bcdafe0b9
perlathebian <perlathebian02@gmail.com>
2026-01-13 22:06:19 +0200

    add factorial function with logging to file

---
+++ week2-core-python-practice/factorial-logging-terminal.py
@@ -0,0 +1,41 @@
+"""Calculates factorial with step-by-step logging to file."""
+
+# # factorialLog
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
+
+# # logging.disable(logging.CRITICAL) # to disable logging
+# logging.debug('Start of program')
+
+# def factorial(n):
+#     logging.debug('Start of factorial(' + str(n) + ')')
+#     total = 1
+#     for i in range(n + 1): # should be for i in range(1, n+1)
+#         total *= i
+#         logging.debug('i is ' + str(i) + ', total is ' + str(total))
+#     logging.debug('End of factorial(' + str(n) + ')')
+#     return total
+
+# print(factorial(5))
+# logging.debug('End of program')
+
+# factorialLog with logging to file instead of terminal
+
+import logging
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG,
+format=' %(asctime)s -  %(levelname)s -  %(message)s')
+
+logging.debug('Start of program')
+
+def factorial(n):
+    logging.debug('Start of factorial(' + str(n) + ')')
+    total = 1
+    for i in range(n + 1): # should be for i in range(1, n+1)
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of factorial(' + str(n) + ')')
+    return total
+
+print(factorial(5))
+logging.debug('End of program')
